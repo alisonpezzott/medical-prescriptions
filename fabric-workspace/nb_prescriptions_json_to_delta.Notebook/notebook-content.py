956d26ed14ed2cf31443460bf3b8df06004a7bf8
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2026-04-28 19:54:36 -0300

    feat: All passed

---
+++ fabric-workspace/nb_prescriptions_json_to_delta.Notebook/notebook-content.py
@@ -0,0 +1,59 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "bbbaff97-36ef-4f05-954f-ca7584c86525",
+# META       "default_lakehouse_name": "lh_medical_prescriptions",
+# META       "default_lakehouse_workspace_id": "0029f683-4d47-4dee-8d97-d14ac0e7205e",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "bbbaff97-36ef-4f05-954f-ca7584c86525"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+from pyspark.sql import functions as F
+
+# 1. Caminho para os arquivos JSON
+path = "Files/medical-prescriptions/*.json"
+
+# 2. Ler os arquivos JSON
+df_raw = spark.read.option("multiline", "true").json(path)
+
+# 3. Transformação: Explodir o array 'medications' e selecionar os campos
+# Usamos F.col("patient.full_name") para acessar subcampos do JSON
+df_final = df_raw.select(
+    F.col("prescription_id"),
+    F.col("issue_date"),
+    F.col("patient.full_name").alias("patient_name"),
+    F.col("patient.patient_id").alias("patient_id"),
+    F.explode(F.col("medications")).alias("medication")
+).select(
+    "*",
+    F.col("medication.name").alias("medication_name"),
+    F.col("medication.dosage").alias("medication_dosage"),
+    F.col("medication.frequency").alias("medication_frequency")
+).drop("medication") # Removemos a coluna temporária do objeto original
+
+# 4. Salvar como uma Tabela Delta no Lakehouse (Tabelas)
+df_final.write.format("delta").mode("overwrite").saveAsTable("medical_prescriptions")
+
+# Mostrar o resultado
+display(df_final)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
